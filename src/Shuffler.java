6d7b39ef9ede01b24cfa5c61716a3284030af585
tcavalluzzi7656 <tcavalluzzi7656@bths.edu>
2019-02-26 14:42:26 -0500

    act 3

--- src/Shuffler.java
+++ src/Shuffler.java
@@ -50,7 +50,9 @@ public class Shuffler {
      * @param values is an array of integers simulating cards to be shuffled.
      */
     public static void perfectShuffle(int[] values) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 3 *** */
+		int[] shuffled = new int[52];
+		int k = 0
+
     }
 
     /**
